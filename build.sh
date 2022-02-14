9c9a1241cda5a87a6bb81dab6df298c15160bffc
Eugenio Laghi <eugenio.laghi@prima.it>
2022-02-14 11:13:10 +0100

    adshuifhsd

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ echo "building ${IMAGE_NAME}"
 source /etc/profile.d/ecs-credentials-endpoint
 # aws ecr-public get-login-password --region us-east-1 | /usr/bin/docker login --username AWS --password-stdin public.ecr.aws
 
-docker run -v $PWD:/code --env CARGO_HOME=/home/app/.cargo localauth0-ci:$DRONE_COMMIT
+docker run -v $PWD:/code --env CARGO_HOME=/home/app/.cargo prima/localauth0-ci:$DRONE_COMMIT
 
 docker build -t "${IMAGE_NAME}" -f Dockerfile_localauth0 "${DOCKERFILE_PATH}"
 CI=true dive "${IMAGE_NAME}"
