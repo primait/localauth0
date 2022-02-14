6378c3e0e2e1d627d8658f0da786729fc06c07f4
Eugenio Laghi <eugenio.laghi@prima.it>
2022-02-14 12:41:54 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -13,9 +13,6 @@ IMAGE_NAME="${IMAGE_PREFIX}${VERSION}"
 echo "building ${IMAGE_NAME}"
 
 source /etc/profile.d/ecs-credentials-endpoint
-# aws ecr-public get-login-password --region us-east-1 | /usr/bin/docker login --username AWS --password-stdin public.ecr.aws
-
-#docker run -v $PWD:/code --env CARGO_HOME=/home/app/.cargo prima/localauth0-ci:$DRONE_COMMIT
 
 docker build -t "${IMAGE_NAME}" -f Dockerfile_localauth0 "${DOCKERFILE_PATH}"
 docker push "${IMAGE_NAME}"
