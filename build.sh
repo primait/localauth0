06045bba1bbb2482e1309fab1ee33054f5a7c03e
Simone Cottini <cottini.simone@gmail.com>
2022-02-15 09:52:42 +0100

    Tag version should be the same as Cargo.toml version

--- build.sh
+++ build.sh
@@ -14,5 +14,12 @@ echo "building ${IMAGE_NAME}"
 
 source /etc/profile.d/ecs-credentials-endpoint
 
+version=$(grep -m1 version Cargo.toml | cut -d'"' -f2)
+
+if [[ "$DRONE_TAG" != "$version" ]] ; then
+    echo "Package version $version does not match release version $DRONE_TAG"
+    exit 1
+fi
+
 docker build -t "${IMAGE_NAME}" -f Dockerfile_localauth0 "${DOCKERFILE_PATH}"
 docker push "${IMAGE_NAME}"
