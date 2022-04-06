0cf120872cfc64a4e0988c0ab1f2b9d328f6bcee
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-04-06 16:30:06 +0200

    refactor permission list

--- web/src/model.rs
+++ web/src/model.rs
@@ -76,9 +76,7 @@ impl Component for Model {
 
                     {{self.permission_input_view()}}
 
-                    <div class="form-grid__row form-grid__row--small">
-                        { for self.permissions.iter().map(|permission| self.view_entry(permission.to_string())) }
-                    </div>
+                    { for self.permissions.iter().map(|permission| self.view_entry(permission.to_string())) }
 
                     <div class="form-grid__row form-grid__row--small">
                         <div class="form-item">
@@ -98,9 +96,7 @@ impl Component for Model {
                     </div>
 
                     <div class="form-grid__row form-grid__row--small">
-                        <div class="button-row">
-                            <button class="button button--primary button--huge" onclick=self.link.callback(|_| Msg::SetPermissions)>{"Generate token"}</button>
-                        </div>
+                        <button class="button button--primary button--huge" onclick=self.link.callback(|_| Msg::SetPermissions)>{"Generate token"}</button>
                     </div>
                 </div>
             </div>
@@ -132,30 +128,34 @@ impl Model {
                     </button>
                 </div>
             </div>
-
-
-            // <div class="form-item">
-            //     <label class="form-label" for="permission">{ "Permission" }</label>
-            //     <div class="form-item__wrapper">
-            //         <div class="form-field">
-            //             <label class="form-field__wrapper">
-            //                 <input id="form-item-name" class="form-field__text" type="text" ref=self.permission_input_ref.clone()/>
-            //             </label>
-            //         </div>
-            //         <div class="form-field">
-            //             <button class="button button--primary button--large " onclick=self.link.batch_callback(|_| { Some(Msg::AddPermission) })>{"+"}</button>
-            //         </div>
-            //     </div>
-            // </div>
         }
     }
 
     fn view_entry(&self, permission: String) -> Html {
         html! {
-            <div class="form-grid__row">
-                <div class="form-grid__row__column form-grid__row__column--small">{&permission}</div>
-                <div class="form-grid__row__column form-grid__row__column--span-3">
-                    <button class="button button--primary button--medium button--icon-only" onclick=self.link.callback(move |_| Msg::RemovePermission(permission.clone()))>{"-"}</button>
+            <div class="form-grid__row form-grid__row--small">
+                <div class="form-grid__row__column form-grid__row__column--span-5">
+                    <div class="form-item">
+                        <div class="form-item__wrapper">
+                            <div class="form-field">
+                                <label class="form-field__wrapper">
+                                    <input id="form-item-name" class="form-field__text" type="text" value={permission.clone()} ref=self.permission_input_ref.clone()/>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-grid__row__column display-grid">
+                    <button
+                        type="button"
+                        class="button button--primary button--huge button--icon-only permission-button"
+                        onclick=self.link.callback(move |_| Msg::RemovePermission(permission.clone()))
+                    >
+                        <div aria-hidden="false" aria-label="Button" class="icon icon--size-s" role="img">
+                            {{self.permission_delete_icon()}}
+                        </div>
+                    </button>
                 </div>
             </div>
         }
@@ -164,6 +164,12 @@ impl Model {
     fn permission_icon(&self) -> Html {
         html! {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M22.281 11.5H12.5V1.719a.5.5 0 1 0-1 0V11.5H1.719a.5.5 0 1 0 0 1H11.5v9.781a.5.5 0 0 0 1 0V12.5h9.781a.5.5 0 0 0 0-1z"></path></svg>
+        }
+    }
+
+    fn permission_delete_icon(&self) -> Html {
+        html! {
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.714 11.976l9.121-9.121a.5.5 0 1 0-.707-.707l-9.121 9.121-9.121-9.121a.5.5 0 0 0-.707.707l9.121 9.121-9.121 9.121a.5.5 0 1 0 .707.707l9.121-9.121 9.121 9.121a.5.5 0 1 0 .707-.707z"></path></svg>
         }
     }
 }
