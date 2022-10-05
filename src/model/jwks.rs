0cb3a69b5dae893fa880e44b5b471bd0f1386e56
Patrick Luca Fazzi <patrick91@live.it>
2022-10-05 10:14:12 +0200

    Add x5c to token (#25)
    * Add x5k to token
    Add x5c field to /.well-known/jwks.json response containing public key in X.509 format.
    The certificate is used for security validation by the official PHP Auth0 library.
    
    Co-authored-by: Mattia Basone <mattia.basone@gmail.com>
    Co-authored-by: Mauro Rocchi <mauro.rocchi@prima.it>

--- src/model/jwks.rs
+++ src/model/jwks.rs
@@ -2,8 +2,14 @@ use std::str::FromStr;
 use std::sync::{RwLock, RwLockWriteGuard};
 
 use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use openssl::asn1::Asn1Time;
+use openssl::bn::{BigNum, MsbOption};
+use openssl::error::ErrorStack;
+use openssl::hash::MessageDigest;
 use openssl::pkey::{PKey, Private};
 use openssl::rsa::Rsa;
+use openssl::x509::extension::{BasicConstraints, KeyUsage, SubjectKeyIdentifier};
+use openssl::x509::{X509NameBuilder, X509};
 use rand::seq::SliceRandom;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
@@ -106,6 +112,7 @@ pub struct Jwk {
     alg: String,
     kid: String,
     r#use: String,
+    x5c: Vec<String>,
     #[serde(skip_serializing)]
     private_key_pem: Vec<u8>,
 }
@@ -119,7 +126,10 @@ impl Jwk {
 
         let modulus: Vec<u8> = rsa.n().to_vec();
         let exponent: Vec<u8> = rsa.e().to_vec();
-        let pkey: PKey<Private> = PKey::from_rsa(rsa)?;
+        let key_pair: PKey<Private> = PKey::from_rsa(rsa)?;
+
+        let x509 = generate_x509_cert(&key_pair)?;
+        let x509cert = base64::encode(x509.to_der()?);
 
         Ok(Self {
             kty: "RSA".to_string(),
@@ -128,7 +138,8 @@ impl Jwk {
             alg: "RS256".to_string(),
             kid: Uuid::new_v4().to_string(),
             r#use: "sig".to_string(),
-            private_key_pem: pkey.private_key_to_pem_pkcs8()?,
+            x5c: vec![x509cert],
+            private_key_pem: key_pair.private_key_to_pem_pkcs8()?,
         })
     }
 
@@ -162,6 +173,41 @@ impl Jwk {
     pub fn rsa(&self) -> Result<Rsa<Private>, Error> {
         Ok(Rsa::private_key_from_pem(&self.private_key_pem)?)
     }
+}
+
+fn generate_x509_cert(key_pair: &PKey<Private>) -> Result<X509, ErrorStack> {
+    let mut x509_name = X509NameBuilder::new()?;
+    x509_name.append_entry_by_text("C", "US")?;
+    x509_name.append_entry_by_text("O", "LocalAuth0 CA")?;
+    x509_name.append_entry_by_text("CN", "LocalAuth0 CA")?;
+    let x509_name = x509_name.build();
+
+    let mut cert_builder = X509::builder()?;
+    cert_builder.set_version(2)?;
+    let serial_number = {
+        let mut serial = BigNum::new()?;
+        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
+        serial.to_asn1_integer()?
+    };
+    cert_builder.set_serial_number(&serial_number)?;
+    cert_builder.set_subject_name(&x509_name)?;
+    cert_builder.set_issuer_name(&x509_name)?;
+    cert_builder.set_pubkey(key_pair)?;
+    let not_before = Asn1Time::days_from_now(0)?;
+    cert_builder.set_not_before(&not_before)?;
+    let not_after = Asn1Time::days_from_now(365)?;
+    cert_builder.set_not_after(&not_after)?;
+
+    cert_builder.append_extension(BasicConstraints::new().critical().ca().build()?)?;
+    cert_builder.append_extension(KeyUsage::new().critical().key_cert_sign().crl_sign().build()?)?;
+
+    let subject_key_identifier = SubjectKeyIdentifier::new().build(&cert_builder.x509v3_context(None, None))?;
+    cert_builder.append_extension(subject_key_identifier)?;
+
+    cert_builder.sign(key_pair, MessageDigest::sha256())?;
+    let cert = cert_builder.build();
+
+    Ok(cert)
 }
 
 #[cfg(test)]
